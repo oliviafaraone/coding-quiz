ec7a19c8448c9fda354358b0316f360cd9973694
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2020-12-20 21:02:01 -0700

    q1 populated

--- assets/script.js
+++ assets/script.js
@@ -1,89 +1,210 @@
-//Starting Quiz to get begin questions
-var page = document.querySelector("#page-content");
-
-  document.getElementById("start-quiz").onclick= function startQuiz(){
-
-      page.querySelector("#header").textContent = "Test Question Input";
-
-      var removeH2 = document.getElementById("desc");
-      removeH2.parentNode.removeChild(removeH2);
-
-      var removeStart = document.getElementById("start-quiz");
-      removeStart.parentNode.removeChild(removeStart);
-
-      //createQuestions();
-
-      createAnswerButtons();
-
-   console.log("testy test test")
-  };
-
-  
 
 // The array of questions for the game.
-var questions = [
+const questions = [
     { q: 'A _______ is a predefined action that we can call or invoke in our code (after we declare it earlier in the code', 
-    a:{ 
-        a: '', 
-        b: '', 
+    answer:{ 
+        a: 'element', 
+        b: 'loop', 
         c: 'function', 
-        d: '' },
-        correctA: '3'
+        d: 'attribute' }, 
+        correctA: 'c'
         },  
 
     { q: 'Which of the following is the appropriate syntax for a for loop?', 
-    a:{ 
-        a: '', 
+    answer:{ 
+        a: 'for([initial expression]; [condition]; [increment expression]) {statement}', 
         b: 'for([initial expression]; [condition]; [increment expression]) {statement}', 
-        c: '', 
-        d: '' },
-        correctA: '2'
+        c: 'for([initial expression]; [condition]; [increment expression]) {statement}', 
+        d: 'for([initial expression]; [condition]; [increment expression]) {statement}' },
+        correctA: 'b'
         }, 
 
     { q: 'What does the return statement do?', 
-    a:{ 
+    answer:{ 
         a: '', 
         b: '', 
         c: '', 
         d: 'it returns a value, but it also ends function execution right then and there' },
-        correctA: '4'
+        correctA: 'd'
     }, 
 
-    { q: 'What syntax would you use to stop the page default of refreshing and removing input page information', 
-    a:{ 
+    { q: 'What syntax would you use to stop the page from refreshing and removing input page information', 
+    answer:{ 
         a: 'event.preventDefault();', 
-        b: '', 
-        c: '', 
-        d: '' },
-        correctA: '1'
+        b: 'event.stopload();', 
+        c: 'event.freezeInput();', 
+        d: 'event.stayPage();' },
+        correctA: 'a'
     }, 
 
     { q: 'With the use of ______, we can set up the click event listener on a parent element and then, through that single event listener, determine which child elements were clicked.', 
-    a:{ 
+    answer:{ 
         a: '', 
         b: '', 
         c: 'event delegation', 
         d: '' },
-        correctA: '3'
+        correctA: 'c'
     } 
   ];
 
-var test= "test";
-  function createAnswerButtons(){
+  //Starting Quiz to get begin questions
+var page = document.querySelector("#page-content");
+
+document.getElementById("start-quiz").onclick= function startQuiz(){
+
+    var removeH2 = document.getElementById("desc");
+    removeH2.parentNode.removeChild(removeH2);
+
+    var removeStart = document.getElementById("start-quiz");
+    removeStart.parentNode.removeChild(removeStart);
+
+    //createQuestions();
+
+    //createAnswerButtons2();
+
+    begQuiz();
+ 
+};
+
+var questionIndex = 0;
+
+function begQuiz() {
+  var firstQuestion = questions[questionIndex];
+  displayQuestion(firstQuestion);
+};
+
+function displayQuestion(question) {
+    var question0 = question.q; 
+    page.querySelector("#header").textContent = question0;
+
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "questions";
+
+     //create answer1 button
+     var answer1El = document.createElement("button");
+     var answer0a = question.answer.a;
+     answer1El.textContent = answer0a;
+     answer1El.className = "btn";
+     actionContainerEl.appendChild(answer1El);
+
+    // create answer2 button
+     var answer2El = document.createElement("button");
+     var answer0b = question.answer.b;
+     answer2El.textContent = answer0b;
+     answer2El.className = "btn";
+     actionContainerEl.appendChild(answer2El);
+
+     // create answer3 button
+     var answer3El = document.createElement("button");
+     var answer0c = question.answer.c;
+     answer3El.textContent = answer0c;
+     answer3El.className = "btn";
+     actionContainerEl.appendChild(answer3El);
+
+     // create answer4 button
+     var answer4El = document.createElement("button");
+     var answer0d = question.answer.d;
+     answer4El.textContent = answer0d;
+     answer4El.className = "btn";
+     actionContainerEl.appendChild(answer4El);
+
+     document.querySelector('#answer-list').appendChild(actionContainerEl);
+
+}
+
+document.getElementById("answer-list").onclick= function nextQuestion(){
+    var userAnswer= ;
+    if (correctA == userAnswer) {
+        document.getElementById("answer").innerHTML = "Correct!";
+    } else {
+        document.getElementById("answer").innerHTML = "Wrong!";
+    }
+};
+
+//function nextQuestion () {
+
+    // grade the user's answer
+    // iterate
+  //  currentQuestionIndex++ 
+  //  var nextQuestion = questions[questionIndex] // the second question
+   // displayQuestion(nextQuestion)
+ // }
+
+
+function createAnswerButtons2(){
+
+    page.querySelector("#header").textContent = questions.q;
+
     var actionContainerEl = document.createElement("div");
     actionContainerEl.className = "questions";
     
      // create answer1 button
      var answer1El = document.createElement("button");
-     //answer1El.textContent = "TEST";
-     //questions.a.a;
-     //answer1El.className = "btn a1-btn";
-     //answer1El.setAttribute('type', 'button');
-     answer1El.innerText="TEST";
+     const a = questions[0]
+     answer1El.textContent = a;
      actionContainerEl.appendChild(answer1El);
-     // create answer2 button
-     console.log('does this work');
+
+    //  // create answer2 button
+    //  var answer2El = document.createElement("button");
+    //  const b = questions.answer.b
+    //  answer2El.textContent = b;
+    //  actionContainerEl.appendChild(answer2El);
+
+    //  // create answer3 button
+    //  var answer3El = document.createElement("button");
+    //  const c = questions.answer.c
+    //  answer3El.textContent = c;
+    //  actionContainerEl.appendChild(answer3El);
+
+    //  // create answer4 button
+    //  var answer4El = document.createElement("button");
+    //  const d = questions.answer.d
+    //  answer4El.textContent = d;
+    //  actionContainerEl.appendChild(answer4El);
+
+
+     document.querySelector('#quiz-questions').appendChild(actionContainerEl)
+     console.log(actionContainerEl);
+     console.log(questions);
      return answer1El;
+    
+;
+  }
+
+
+
+      
+  function createAnswerButtons(){
+
+    page.querySelector("#header").textContent = "A _______ is a predefined action that we can call or invoke in our code (after we declare it earlier in the code";
+
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "questions";
+    
+     // create answer1 button
+     var answer1El = document.createElement("button");
+     answer1El.textContent = "element";
+     actionContainerEl.appendChild(answer1El);
+
+     // create answer2 button
+     var answer2El = document.createElement("button");
+     answer2El.textContent = "loop";
+     actionContainerEl.appendChild(answer2El);
+
+     // create answer3 button
+     var answer3El = document.createElement("button");
+     answer3El.textContent = "function";
+     actionContainerEl.appendChild(answer3El);
+
+     // create answer4 button
+     var answer4El = document.createElement("button");
+     answer4El.textContent = "attribute";
+     actionContainerEl.appendChild(answer4El);
+
+
+     document.querySelector('#quiz-questions').appendChild(actionContainerEl)
+     console.log(actionContainerEl);
+    //  return answer1El;
     
 
   };
@@ -186,27 +307,3 @@ var test= "test";
 
   
 
-
-  // We start the game with a score of 0.
-  //var score = 0;
-  
-  // Loop over every question object
-//   for (var i = 0; i < questions.length; i++) {
-//     // Display current question to user and ask OK/Cancel
-//     //var answer = confirm(questions[i].q);
-//     var answer = 
-  
-//     // Compare answers
-//     if (
-//       (answer === true && questions[i].a === 't') ||
-//       (answer === false && questions[i].a === 'f')
-//     ) {
-//       // Increase score
-//       score++;
-//       // Alert the user
-//       alert('Correct!');
-//     } else {
-//       alert('Wrong!');
-//     }
-//   }
-  
